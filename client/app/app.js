22df6d1ecee263b82224706db15cd8df9aa9c5fb
kkinaman <kkinaman419@gmail.com>
2016-10-14 19:33:29 -0700

    Add link, sign up, and sign in form validation

--- client/app/app.js
+++ client/app/app.js
@@ -60,11 +60,8 @@ angular.module('shortly', [
   // when it does change routes, we then look for the token in localstorage
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
-  console.log('HELLLOOOO');
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-    // console.log('----------next.$$route', next.$$route);
-    // console.log('----------next.$$route.authenticate', next.$$route.authenticate);
       $location.path('/signin');
     }
   });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -4,11 +4,15 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = '';
-  $scope.addLink = function() {
-    var newLink = {
-      url: $scope.link
-    };
-    Links.addOne(newLink);
-    $scope.link = '';
+  $scope.addLink = function(isValid) {
+    if (isValid) {
+      var newLink = {
+        url: $scope.link
+      };
+      Links.addOne(newLink);
+      $scope.link = '';
+    }
   };
+
+  $scope.regexValid = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 });
